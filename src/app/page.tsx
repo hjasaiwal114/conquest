a4edac6c34f8e82177aa28ef8ce03e75c8e64374
hjasaiwal114 <hjasaiwal114@gmail.com>
2024-11-13 11:33:05 -0800

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client"
+
+import { Button } from "@nextui-org/react";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl">hello</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
